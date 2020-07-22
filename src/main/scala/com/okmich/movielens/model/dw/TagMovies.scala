554b27b43eb60d4a4571ae21c5675386bd7bd554
GGhosh <GGhosh@in.imsheatlh.com>
2020-07-22 19:55:36 +0530

    test upload

--- src/main/scala/com/okmich/movielens/model/dw/TagMovies.scala
+++
@@ -1,9 +0,0 @@
-package com.okmich.movielens.model.dw
-
-case class TagMovies(tag : String,
-                      movies : Array[Int]) {
-
-  private val SEP = "|"
-  override def toString: String = "[ "+this.tag + SEP + this.movies.mkString(SEP) + " ]"
-
-}
