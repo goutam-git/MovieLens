50f919c13139c467d39a9b733a104a827847782b
GGhosh <GGhosh@in.imsheatlh.com>
2020-10-28 14:55:40 +0530

    Changes to original Movies

--- src/test/scala/com/okmich/movielens/jobs/dw/TransFormMoviesTest.scala
+++ src/test/scala/com/okmich/movielens/jobs/dw/TransFormMoviesTest.scala
@@ -1,22 +1,22 @@
 package com.okmich.movielens.jobs.dw
 
-import java.sql.Timestamp
+
+import java.time.Instant
 
 import com.okmich.movielens.jobs.JobTest
 import com.okmich.movielens.model.da.{Movies, Ratings}
 import com.okmich.movielens.model.dw.MovieTypes
 import org.apache.spark.sql.{DataFrame, Dataset, Encoder, Encoders}
-import org.mockito.Mockito._
 import org.mockito.Mockito
+import org.mockito.Mockito._
 
 
 class TransFormMoviesTest  extends JobTest{
-  val now: Long = System.currentTimeMillis()
-  val nowDate: Int = 20200701
-  val nowTimestamp: Timestamp = new Timestamp(now)
+
+  val now: Long = Instant.now().getEpochSecond();
   implicit val movTypEncoder: Encoder[MovieTypes] = Encoders.product[MovieTypes]
   implicit val movEncoder: Encoder[Movies] = Encoders.product[Movies]
-  implicit val ratingsEncoder: Encoder[Ratings] = Encoders.product[Ratings]
+
 
   "TransFormMovies" should " return  movie dataset by extracting the genres from Movies and Year from Ratings" in {
 
@@ -25,19 +25,14 @@ class TransFormMoviesTest  extends JobTest{
       Movies("2","Jumanji (1995)","Adventure|Children|Fantasy")
    )
 
-   val ratingsList: List[Ratings]  = List(
-     Ratings("1","1","3.5",nowTimestamp),
-     Ratings("1","2","3.5",nowTimestamp)
-   )
+
 
     val moviesDS : Dataset[Movies] = createDataSet[Movies](movieList)
 
-    val ratingsDS: Dataset[Ratings] = createDataSet[Ratings](ratingsList)
-
 
     val output : List[MovieTypes] = List(
-      MovieTypes("2", "Jumanji (1995)", 2020,false,true,false,true,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false),
-      MovieTypes("1", "Toy Story (1995)", 2020,false,true,true, true,true,false,false,false,true,false,false,false,false,false,false,false,false,false,false)
+      MovieTypes("1", "Toy Story (1995)",1995,false,true,true, true,true,false,false,false,true,false,false,false,false,false,false,false,false,false,false),
+      MovieTypes("2", "Jumanji (1995)",1995,false,true,false,true,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false)
     )
 
     val movieDF : DataFrame = createDataFrame(movieList)
@@ -45,7 +40,6 @@ class TransFormMoviesTest  extends JobTest{
 
     System.err.println("Movies DataFrame" + printDataFrame(movieDF))
     System.err.println("Movies Dataset: " + printDataSet[Movies](moviesDS))
-    System.err.println("Ratings Dataset: " + printDataSet[Ratings](ratingsDS))
     System.err.println("Expected Output: " + printDataSet[MovieTypes](expectedOutput))
 
 
@@ -53,11 +47,10 @@ class TransFormMoviesTest  extends JobTest{
     val mockJob = mock(classOf[TransFormMovies],withSettings().spiedInstance(job).serializable
       .defaultAnswer(Mockito.CALLS_REAL_METHODS))
     doReturn(moviesDS).when(mockJob).readCsv[Movies]("testpath")
-    doReturn(ratingsDS).when(mockJob).readCsv[Ratings]("testpath")
 
-    val result :  Dataset[MovieTypes]  = mockJob.transformMovies(moviesDS,ratingsDS)
+    val result :  Dataset[MovieTypes]  = mockJob.transformMovies(moviesDS)
 
-    System.err.println("result: " + printDataSet[MovieTypes](result))
+    System.err.println("RESULT-DATASET: " + printDataSet[MovieTypes](result))
 
     result.collect should contain theSameElementsAs expectedOutput.collect
 
